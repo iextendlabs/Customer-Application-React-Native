43156efc98d6d06b2717e86234ca72a645ec3457
Ali Hamza <86736070+Hamza00003@users.noreply.github.com>
2024-03-07 21:27:26 +0500

    Update Booking.js

--- src/Bottom/Booking.js
+++ src/Bottom/Booking.js
@@ -123,7 +123,7 @@ export default function Booking() {
     setSelectedArea(item);
 
     if (selectedDate) {
-      (selectedDate, item);
+      fetchAvailableTimeSlots(selectedDate, item);
     }
   };
 
